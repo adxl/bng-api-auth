91ec252b2185fdb02d11b7d94257dc2d2de59497
Adel Sen <adelsenhadjii@gmail.com>
2023-05-25 03:09:29 +0200

    Feat: implement Sentry

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { RpcException } from '@nestjs/microservices';
 
 @Injectable()
 export class AppService {
   index(): string {
+    throw new RpcException(new ForbiddenException('Not Authorized !'));
     return 'Auth API';
   }
 }
