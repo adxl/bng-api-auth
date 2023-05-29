47174c176747bd6268a563e383b94aff09ca6097
Adel Sen <adelsenhadjii@gmail.com>
2023-05-29 19:38:31 +0200

    Feat: add local development alternative

--- src/domains/users/users.module.ts
+++ src/domains/users/users.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { User } from './users.entity';
-import { Role } from './role.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
